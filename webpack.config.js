51c352d0de73b8c4a6efdef8cb9e0e47c4977270
yangguangyuan <yangguangyuan@hzboc.com>
2016-08-23 17:05:04 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
         index: './src/main.js',
         header1:'./src/js/header/header1.js',
         header2:'./src/js/header/header2.js',
-        banner1:'./src/js/banner/1.js'
+        banner1:'./src/js/banner/1.js',
+        banner2:'./src/js/banner/2.js'
     },
     //入口文件输出配置
     output: {
@@ -18,6 +19,7 @@ module.exports = {
         //加载器配置
         loaders: [
             {test: /\.vue$/, loader: 'vue'},
+            {test: /\.less/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")},
             {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader','css-loader')},
             {test: /\.js$/, loader: 'babel-loader'},
             {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
